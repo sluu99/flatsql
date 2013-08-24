da9f38eca9113651739086f7e82e9a45d147be09
sluu99 <sluu99@gmail.com>
2013-08-24 13:47:07 -0500

    Added support for travis-ci

--- src/test/java/flatsql/test/TestHelper.java
+++ src/test/java/flatsql/test/TestHelper.java
@@ -9,21 +9,44 @@ import java.sql.DriverManager;
  */
 public class TestHelper {
 
-    private static final String TABLE_NAME = String.format("flatsql_test_%d", System.currentTimeMillis());
-    private static final String BASE_CONNECTION_STRING = "jdbc:mysql://localhost/?user=root";
-    private static final String CONNECTION_STRING = String.format("jdbc:mysql://localhost/%s?user=root", TABLE_NAME);
+    private static String DB_NAME;
+    private static String DB_USER;
+    private static String BASE_CONNECTION_STRING;
+    private static String CONNECTION_STRING;
+    private static boolean IS_TRAVIS = false;
+
+
+    static {
+        // support for travis CI
+        if (System.getenv("TRAVIS") != null) {
+            IS_TRAVIS = true;
+            DB_NAME = "myapp_test";
+            DB_USER = "travis";
+        } else {
+            DB_NAME = String.format("flatsql_test_%d", System.currentTimeMillis());
+            DB_USER = "root";
+        }
+        BASE_CONNECTION_STRING = String.format("jdbc:mysql://127.0.0.1/?user=%s", DB_USER);
+        CONNECTION_STRING = String.format("jdbc:mysql://127.0.0.1/%s?user=%s", DB_NAME, DB_USER);
+    }
 
     public static void createTestDatabase() throws Exception {
+        if (IS_TRAVIS) {
+            return;
+        }
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(BASE_CONNECTION_STRING);
-        conn.createStatement().execute("CREATE DATABASE " + TABLE_NAME);
+        conn.createStatement().execute("CREATE DATABASE " + DB_NAME);
         conn.close();
     }
 
     public static void dropTestDatabase() throws Exception {
+        if (IS_TRAVIS) {
+            return;
+        }
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(BASE_CONNECTION_STRING);
-        conn.createStatement().execute("DROP DATABASE " + TABLE_NAME);
+        conn.createStatement().execute("DROP DATABASE " + DB_NAME);
         conn.close();
     }
 
