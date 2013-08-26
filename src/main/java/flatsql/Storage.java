fb4f079ccf490bd2fe33c94010ad87fc685b789b
sluu99 <sluu99@gmail.com>
2013-08-26 00:08:37 -0500

    Storing attributes now support array of primitive types

--- src/main/java/flatsql/Storage.java
+++ src/main/java/flatsql/Storage.java
@@ -1,10 +1,12 @@
 package flatsql;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
@@ -74,7 +76,7 @@ final class Storage {
 		String attrSql = String.format(INSERT_ATTR_SQL, tableName);
 
 		PreparedStatement deleteStmt = null;
-		PreparedStatement[] stmts = new PreparedStatement[getters.size()];
+		ArrayList<PreparedStatement> stmts = new ArrayList<>(); 
 
 		try {
 			// delete the old attributes
@@ -84,29 +86,43 @@ final class Storage {
 
 			// insert each of the new attributes
 			Iterator<Entry<String, Method>> it = getters.entrySet().iterator();
-			int i = 0;
+			
 			while (it.hasNext()) {
-				Entry<String, Method> pair = it.next();
+				Entry<String, Method> pair = it.next();				
+				Method getter = pair.getValue();
+				boolean isArray = getter.getReturnType().isArray(); 
+				String values[] = getValues(entity, getter);
+				
+				// only arrays should have more than 1 value
+				for (int i = 0; i < values.length; i++) {
+					PreparedStatement stmt = conn.prepareStatement(attrSql);
+					stmt.setString(1, entity.id());					
+					stmt.setString(3, values[i]);
+					stmt.setString(4, null);
 
-				stmts[i] = conn.prepareStatement(attrSql);
-				stmts[i].setString(1, entity.id());
-				stmts[i].setString(2, pair.getKey());
-				stmts[i].setString(3, getValue(entity, pair.getValue()));
-				stmts[i].setString(4, null);
-
-				stmts[i].execute();
-				i++;
+					if (isArray) {
+						stmt.setString(2, String.format("%s[%d]", pair.getKey(), i));
+					} else {
+						stmt.setString(2, pair.getKey());
+					}
+					
+					stmt.execute();
+					
+					stmts.add(stmt);
+				}
 			}
 
 		} catch (SQLException e) {
 			throw e;
 		} finally {
 			JdbcUtil.close(null, deleteStmt);
-			for (int i = 0; i < stmts.length; i++) {
-				JdbcUtil.close(null, stmts[i]);
+			
+			for (PreparedStatement stmt : stmts) {
+				JdbcUtil.close(null, stmt);
 			}
 		}
 	}
+	
 	
 	/**
 	 * Persist an object's attribute into the database
@@ -193,20 +209,43 @@ final class Storage {
 	 * 
 	 * @param entity
 	 * @param m
-	 * @return
+	 * @return An array of values.
+	 * 		If the method (m) does not return an array then the return value has length of 1.
+	 * 		If the method returns an array but it's either empty or null, then the return value will be an empty array. 
 	 */
-	private static String getValue(Entity entity, Method m) {
+	private static String[] getValues(Entity entity, Method m) {
 
 		if (entity == null || m == null) {
 			return null;
 		}
-
+		
+		boolean isArray = false;
 		Class<?> returnType = m.getReturnType();
+		if (returnType.isArray()) {
+			isArray = true;
+			returnType = returnType.getComponentType();
+		}
+		
 		Object objVal;
 
 		try {
 			objVal = m.invoke(entity);
-			return convertToString(objVal, returnType);
+			
+			if (!isArray) {			
+				return new String[] { convertToString(objVal, returnType) };
+			} else { // returning an array
+				if (objVal != null) {
+					String[] values = new String[Array.getLength(objVal)];
+					
+					for (int i = 0; i < values.length; i++) {
+						values[i] = convertToString(Array.get(objVal, i), returnType);
+					}
+					
+					return values;
+				} else { // the array-getter returns null
+					return new String[0];
+				}				
+			}
 		} catch (IllegalAccessException | IllegalArgumentException
 				| InvocationTargetException e) {
 			return null;
